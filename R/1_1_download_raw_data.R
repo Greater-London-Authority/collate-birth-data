a0f7147df42a2d31ea54a5b4ade58859de6b6d97
Marta Lapsley <marta.lapsley@london.gov.uk>
2024-01-19 16:23:29 +0000

    moved raw data URLs from inside funtions to a lookup file

--- R/1_1_download_raw_data.R
+++ R/1_1_download_raw_data.R
@@ -8,11 +8,15 @@ fpath <- list(dir_raw = "data/raw/",
               raw_births_cy_lsoa = "data/raw/births_calendar_year_nomis_lsoa.rds",
               raw_births_my_lsoa = "data/raw/births_mid_year_lsoa_1991_2017.xlsx",
               raw_mye_coc = "data/raw/mye_coc.csv"
-              )
+)
 
 urls <- list(mye_coc = "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/populationandmigration/populationestimates/datasets/populationestimatesforukenglandandwalesscotlandandnorthernireland/mid2021/dataforreconciliation.zip",
              births_my_lsoa = "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/birthsdeathsandmarriages/deaths/adhocs/009628birthsanddeathsbylowersuperoutputarealsoaenglandandwales1991to1992to2016to2017/lsoabirthsdeaths19912017final.zip"
-             )
+)
+
+monthly_births_urls <- read.csv("lookups/monthly_births_data_urls.csv", stringsAsFactors = FALSE) %>%
+  pull(url)
+monthly_births_fps <- paste0("data/raw/monthly_births/", basename(monthly_births_urls))
 
 if(!dir.exists(fpath$dir_raw)) dir.create(fpath$dir_raw, recursive = TRUE)
 
@@ -29,9 +33,11 @@ fetch_mye_births(fp_save = fpath$raw_mye_coc,
                  url_zip = urls$mye_coc
 )
 
-# get monthly births from ONS adhoc pages (different page for each year, URLS are specified inside function)
-fetch_monthly_births_la(dir_save = "data/raw/monthly_births/"
-)
+# get monthly births from ONS adhoc pages
+for (i in seq_along(monthly_births_urls)) {
+  fetch_monthly_births_la(url = monthly_births_urls[[i]],
+                          fp_save = monthly_births_fps[[i]])
+}
 
 #2020 MYE file - now superseded
 #"https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/populationandmigration/populationestimates/datasets/populationestimatesforukenglandandwalesscotlandandnorthernireland/mid2001tomid2020detailedtimeseries/ukdetailedtimeseries2001to2020.zip"
--- R/functions/fetch_monthly_births_la.R
+++ R/functions/fetch_monthly_births_la.R
@@ -1,32 +1,14 @@
 fetch_monthly_births_la <- function(
-    dir_save = "data/raw/monthly_births/"
+    url,
+    fp_save
 ) {
 
-
-  urls <- list(
-    sept14_aug15 = "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/birthsdeathsandmarriages/livebirths/adhocs/006871livebirthsbymonthsexandareaofusualresidenceofmotherenglandandwalesseptember2014toaugust2015/livebirthsbymonthsexandladsept2014toaug2015ew.xls",
-    sept15_aug16 = "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/birthsdeathsandmarriages/livebirths/adhocs/008449livebirthsbymonthsexandareaofusualresidenceofmotherenglandandwalesseptember2015toaugust2016/livebirthsbymonthsexandladsept2015toaug2016ew.xls",
-    sept16_aug17 = "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/birthsdeathsandmarriages/livebirths/adhocs/009923livebirthsbymonthsexandareaofusualresidenceofmotherenglandandwalesseptember2016toaugust2017final/livebirthsbymonthsexandladsept2016toaug2017ew.xls",
-    sept17_aug18 = "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/birthsdeathsandmarriages/livebirths/adhocs/11467livebirthsbymonthsexandareaofusualresidenceofmotherenglandandwalesseptember2017toaugust2018/livebirthsbymonthsexandladsept2017toaug2018.xls",
-    sept18_aug19 = "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/birthsdeathsandmarriages/livebirths/adhocs/13050livebirthsbymonthsexandareaofusualresidenceofmotherenglandandwalesseptember2018toaugust2019/livebirthsbymonthsexandladsept2018toaug2019.xlsx",
-    sept19_aug20 = "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/birthsdeathsandmarriages/livebirths/adhocs/14756livebirthsbymonthsexandareaofusualresidenceofmotherenglandandwalesseptember2019toaugust2020/copyoflivebirthsbymonthsexandladsept2019toaug2020.xlsx",
-    sept20_aug21 = "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/birthsdeathsandmarriages/livebirths/adhocs/1077livebirthsbymonthsexandareaofusualresidenceofmotherenglandandwalesseptember2020toaugust2021/livebirthsbymonthsexandladsept2020toaug2021final.xlsx"
-    )
-
+  dir_save = dirname(fp_save)
   if (!dir.exists(dir_save)) {dir.create(dir_save)}
 
-  fps <- lapply(urls, function(x){paste0(dir_save, basename(x))})
 
-  for (i in seq_along(urls)) {
-
-    download.file(urls[[i]],
-                  destfile = fps[[i]],
-                  mode="wb")
-  }
-
-
-
-# file.rename(from = paste0(dir_save, "Table_1_Births.xlsx"),
-#              to = fp_save)
+  download.file(url,
+                destfile = fp_save,
+                mode="wb")
 
 }
